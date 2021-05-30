2e443db850369210ba7d22e883fe802da3d9d4a7
coloryourlife <95chris0319@gmail.com>
2021-05-30 20:34:56 +0900

    add main page

--- app/src/main/java/com/uosmobile/team1/MainActivity.java
+++ app/src/main/java/com/uosmobile/team1/MainActivity.java
@@ -1,13 +1,58 @@
 package com.uosmobile.team1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment0, fragment1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment0 = new Fragment0();
+        fragment1 = new Fragment1();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, fragment0).commit();
+
+        TabLayout tabs = (TabLayout)findViewById(R.id.BookList);
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Fragment selected = null;
+                if(position == 0){
+                    selected = fragment0;
+                } else if (position == 1){
+                    selected = fragment1;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+
     }
 }
