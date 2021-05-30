22c4179ced881d1af4e6c489ce9862fa83733781
coloryourlife <95chris0319@gmail.com>
2021-05-30 22:40:06 +0900

    add Bookadapter and Bookdata for recyclerview

---
+++ app/src/main/java/com/uosmobile/team1/BookData.java
@@ -0,0 +1,13 @@
+package com.uosmobile.team1;
+
+public class BookData {
+    private String title;
+
+    public BookData(int ic_launcher, String title){
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
--- app/src/main/java/com/uosmobile/team1/Fragment0.java
+++ app/src/main/java/com/uosmobile/team1/Fragment0.java
@@ -8,13 +8,39 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Fragment0 extends Fragment {
-    @Nullable
+
+    private ArrayList<BookData> bookList;
+    private BookAdapter bookAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment0, container, false);
-
+        recyclerView = (RecyclerView) view.findViewById(R.id.BookList);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.scrollToPosition(0);
+        bookAdapter = new BookAdapter(bookList);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
     }
 }
