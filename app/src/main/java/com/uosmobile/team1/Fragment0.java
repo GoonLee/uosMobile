f917300e436b74e5192090f6d9177d9a0da1e7e1
coloryourlife <95chris0319@gmail.com>
2021-06-03 20:45:43 +0900

    [WIP]add load func to read the title of the books from the storage

--- app/src/main/java/com/uosmobile/team1/Fragment0.java
+++ app/src/main/java/com/uosmobile/team1/Fragment0.java
@@ -1,6 +1,8 @@
 package com.uosmobile.team1;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Fragment0 extends Fragment {
@@ -20,6 +23,8 @@ public class Fragment0 extends Fragment {
     private BookAdapter bookAdapter;
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
+    private String ext = Environment.getExternalStorageState();
+    private String sdPath;
 
 
     @Override
@@ -43,7 +48,7 @@ public class Fragment0 extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        initDataset();
+        load();
     }
 
     private void initDataset(){
@@ -52,5 +57,32 @@ public class Fragment0 extends Fragment {
         bookList.add(new BookData("개구리 소년"));
         bookList.add(new BookData("신데렐라"));
         bookList.add(new BookData("촉법소년"));
+    }
+
+    public void load(){
+        bookList = new ArrayList<BookData>();
+        String sdPath;
+        String title;
+        String ext = Environment.getExternalStorageState();
+        if(ext.equals(Environment.MEDIA_MOUNTED)){
+            sdPath = Environment.getExternalStoragePublicDirectory("Download").getAbsolutePath();
+            Log.d("dd",sdPath);
+        } else {
+            sdPath = Environment.MEDIA_UNMOUNTED;
+        }
+        try {
+            File bookTitle = new File(sdPath, "Contents");
+
+            Log.d("file", bookTitle.getAbsolutePath());
+
+            for (File f : bookTitle.listFiles()) {
+                if (f.isFile()) {
+                    title = f.getName();
+                    bookList.add(new BookData(title));
+                }
+            }
+        } catch (Exception e) {
+            Log.i("에러: ", e.getMessage());
+        }
     }
 }
