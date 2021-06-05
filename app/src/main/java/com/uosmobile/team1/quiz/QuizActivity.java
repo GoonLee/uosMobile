b6a2e1a0647e970efef853790ee122219cd4e726
GoonLee <auteur.kor@gmail.com>
2021-06-05 16:17:48 +0900

    Add placeholders

---
+++ app/src/main/java/com/uosmobile/team1/quiz/QuizActivity.java
@@ -0,0 +1,14 @@
+package com.uosmobile.team1.quiz;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.uosmobile.team1.R;
+
+public class QuizActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
---
+++ app/src/main/java/com/uosmobile/team1/stamp/StampFragment.java
@@ -0,0 +1,21 @@
+package com.uosmobile.team1.stamp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.uosmobile.team1.R;
+
+public class StampFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_stamp, container, false);
+
+        return view;
+    }
+}
